0647fb1f3aadab740052417fa6bcebe0df6796a2
xcham26 <chaloupm@gmail.com>
2013-06-10 19:26:20 +0300

    Create new.java

---
+++ druhy/src/new.java
@@ -0,0 +1,14 @@
+
+
+public class new {
+
+  /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//https://github.com/xcham26/xcham26Repo.git
+
+	}
+
+}
