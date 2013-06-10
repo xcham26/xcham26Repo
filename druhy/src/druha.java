ef2ef78932696e404b7f8f3b14b98fcda0c00e72
xcham26 <chaloupm@gmail.com>
2013-06-10 20:06:05 +0300

    sewwww

--- druhy/src/druha.java
+++ druhy/src/druha.java
@@ -8,19 +8,7 @@ public class druha {
 		//https://github.com/xcham26/xcham26Repo.git
 		
 		
-		//hdsgfukszhd
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//hdsgfukszhd
+	//aaaaaaaaaaaaaaaaaaaaaaaa
 
 	}
 
